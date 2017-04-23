67c35c09c53df48ba1f1372f711301f0557a13c2
Sanket Sharma <sanket.sharma@dukstra.com>
2017-04-23 21:51:17 +0200

    Updated configuration injection

--- addon/instance-initializers/mapbox-gl.js
+++ addon/instance-initializers/mapbox-gl.js
@@ -1,6 +1,6 @@
 
-export function initialize(application) {
-  let config = application.container.lookupFactory('config:environment');
+export function initialize(applicationInstance) {
+  let config = applicationInstance.application;
 
   if (!config.mapbox || !config.mapbox.accessToken) {
     console.error('Please specify your mapbox.accessToken in your config.');
