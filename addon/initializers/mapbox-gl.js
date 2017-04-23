5900a75ca687a33d9371044c0c6c22ea8fc0eb2b
Sanket Sharma <sanket.sharma@dukstra.com>
2017-04-23 20:42:19 +0200

    Test

--- addon/initializers/mapbox-gl.js
+++ addon/initializers/mapbox-gl.js
@@ -1,7 +1,7 @@
 /**
  * Created by sanketsharma on 2017/04/23.
  */
-import MapboxGl from 'ember-cli-mapbox-gl-js/services/mapbox';
+import MapboxGl from 'ember-cli-mapbox-gl-js/services/mapbox-gl';
 
 export default {
   name: 'mapbox-gl-service',
