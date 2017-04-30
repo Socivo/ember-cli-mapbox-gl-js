f772dde9f164a7de03ce57eaf84cf1c6b9d13cb3
Sanket Sharma <sanket.sharma@dukstra.com>
2017-04-30 15:12:40 +0200

    The  method for Application initializer 'mapbox-gl-service' should take only one argument - , an instance of an .

--- addon/initializers/mapbox-gl.js
+++ addon/initializers/mapbox-gl.js
@@ -6,7 +6,7 @@ import MapboxGl from 'ember-cli-mapbox-gl-js/services/mapbox-gl';
 export default {
   name: 'mapbox-gl-service',
 
-  initialize: function(container, application) {
+  initialize: function(application) {
     application.register('service:mapbox-gl', MapboxGl);
   }
 };
