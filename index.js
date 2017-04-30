73cf62785d0b11d723475d5fad1c8a3a51b0653e
Sanket Sharma <sanket.sharma@dukstra.com>
2017-04-30 14:04:25 +0200

    Fixed importing of mapbox files

--- index.js
+++ index.js
@@ -6,28 +6,57 @@ var Funnel = require('broccoli-funnel');
 
 module.exports = {
   name: 'ember-cli-mapbox-gl-js',
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+    //auto import required files for mapbox
+    let mapboxglJS = path.join(this.app.project.root, 'node_modules', 'mapbox-gl', 'dist');
+    let mapboxSVG = path.join(this.app.project.root, 'node_modules', 'mapbox-gl', 'dist', 'svg');
+    let svgTree = new Funnel(mapboxSVG, {
+      include: ['*.svg'],
+      destDir: 'svg'
+
+    });
+
+    this.app.import(path.join(mapboxglJS, 'mapbox-gl.js'));
+    this.app.import(path.join(mapboxglJS, 'mapbox-gl.css'));
+    this.app.import(svgTree);
+
+
+
+  },
+  /**
+   * Make JS files available for importing.
+   * @param tree
+   * @returns Vendor Tree
+   */
 
   treeForVendor: function (tree) {
 
     var mapboxglJS = path.join(this.app.project.root, 'node_modules', 'mapbox-gl', 'dist');
     var vendorTree = new Funnel(mapboxglJS, {
-      include: ['mapbox-gl.js']
+      files: ['mapbox-gl.js']
     });
 
     return mergeTrees([vendorTree]);
   },
   treeForStyles: function (tree) {
     var mapboxCSS = path.join(this.app.project.root, 'node_modules', 'mapbox-gl', 'dist');
-    var mapboxSVG = path.join(this.app.project.root, 'node_modules', 'mapbox-gl', 'dist', 'svg');
+
     var cssTree = new Funnel(mapboxCSS, {
-      include: ['mapbox-gl.css']
+      files: ['mapbox-gl.css']
     });
-    var svgTree = new Funnel(mapboxSVG, {
+
+    return mergeTrees([cssTree, svgTree]);
+
+  },
+  treeForPublic: function(tree) {
+    let mapboxSVG = path.join(this.app.project.root, 'node_modules', 'mapbox-gl', 'dist', 'svg');
+    let svgTree = new Funnel(mapboxSVG, {
       include: ['*.svg'],
       destDir: 'svg'
 
     });
-    return mergeTrees([cssTree, svgTree]);
+    return svgTree;
 
   }
 };
